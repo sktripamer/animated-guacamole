59c9369c38a8ecfaa3cb68ed89683db402a5d078
sktripamer <42321951+sktripamer@users.noreply.github.com>
2021-11-25 20:01:51 -0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,81 +1,151 @@
-import React from "react"
-import { useForm } from "react-hook-form"
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {loadStripe} from '@stripe/stripe-js';
+import {
+  CardElement,
+  Elements,
+  useStripe,
+  useElements,
+} from '@stripe/react-stripe-js';
 
-export default function App() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm()
-  const onSubmit = data => {
-    fetch(`/api/form`, {
-      method: `POST`,
-      body: JSON.stringify(data),
-      headers: {
-        "content-type": `application/json`,
+const CheckoutForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+
+  // we will edit this
+  const handleSubmit = async (event) => {
+
+    const intent = await fetch(`/api/payment-intent`, {
+      method: "POST",
+      body: JSON.stringify({
+          amount: 500,
+        }),
+      });
+    const { paymentIntent } = await intent.json();
+
+    await stripe.confirmCardPayment(paymentIntent.client_secret, {
+      payment_method: {
+        card: elements.getElement(CardElement),
+        billing_details: {
+          email: profile.email,
+        },
       },
-    })
-      .then(res => res.json())
-      .then(body => {
-        console.log(`response from API:`, body)
-      })
-  }
+    });
 
-  console.log({ errors })
+    event.preventDefault();
+    const {error, paymentMethod} = await stripe.createPaymentMethod({
+      type: 'card',
+      card: elements.getElement(CardElement),
+    });
+
+
+
+  };
+
+
+// handleSubmit()
+
+
 
   return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      style={{ display: `block`, width: 400 }}
-    >
-      <label htmlFor="first-name">First name</label>
-      <input
-        id="first-name"
-        type="text"
-        style={{ display: `block`, marginBottom: 16 }}
-        {...register("First name", { required: true, maxLength: 80 })}
-      />
-
-      <label htmlFor="last-name">Last name</label>
-      <input
-        id="last-name"
-        type="text"
-        style={{ display: `block`, marginBottom: 16 }}
-        {...register("Last name", { required: true, maxLength: 100 })}
-      />
-
-      <label htmlFor="email">Email</label>
-      <input
-        id="email"
-        type="text"
-        style={{ display: `block`, marginBottom: 16 }}
-        {...register("Email", { required: true, pattern: /^\S+@\S+$/i })}
-      />
-
-      <label htmlFor="tel">Mobile number</label>
-      <input
-        id="tel"
-        type="tel"
-        style={{ display: `block`, marginBottom: 16 }}
-        {...register("Mobile number", {
-          required: true,
-          minLength: 6,
-          maxLength: 12,
-        })}
-      />
-
-      <label htmlFor="title">Title</label>
-      <select
-        {...register("Title", { required: true })}
-        style={{ display: `block`, marginBottom: 16 }}
-      >
-        <option value="Mr">Mr</option>
-        <option value="Mrs">Mrs</option>
-        <option value="Miss">Miss</option>
-        <option value="Dr">Dr</option>
-      </select>
-
-      <input type="submit" />
+    <form onSubmit={handleSubmit}>
+      <CardElement /> 
+      <button type="submit" disabled={!stripe}>
+        Pay
+      </button>
     </form>
-  )
-}
+  );
+};
+
+const stripePromise = loadStripe('pk_test_51Jr6IuEIi9OXKxaBdi4aBOlRU6DgoMcQQNgDCOLo1p8TZDy29xR5tKWHP5C02bF7kKHpkWKq9DI9OCzClVtj8zi500XedIOBD3');
+
+const App = () => (
+  <Elements stripe={stripePromise}>
+    <CheckoutForm />
+  </Elements>
+);
+
+ReactDOM.render(<App />, document.body);
+
+
+// import React from "react"
+// import { useForm } from "react-hook-form"
+
+// export default function App() {
+//   const {
+//     register,
+//     handleSubmit,
+//     formState: { errors },
+//   } = useForm()
+//   const onSubmit = data => {
+//     fetch(`/api/form`, {
+//       method: `POST`,
+//       body: JSON.stringify(data),
+//       headers: {
+//         "content-type": `application/json`,
+//       },
+//     })
+//       .then(res => res.json())
+//       .then(body => {
+//         console.log(`response from API:`, body)
+//       })
+//   }
+
+//   console.log({ errors })
+
+//   return (
+//     <form
+//       onSubmit={handleSubmit(onSubmit)}
+//       style={{ display: `block`, width: 400 }}
+//     >
+//       <label htmlFor="first-name">First name</label>
+//       <input
+//         id="first-name"
+//         type="text"
+//         style={{ display: `block`, marginBottom: 16 }}
+//         {...register("First name", { required: true, maxLength: 80 })}
+//       />
+
+//       <label htmlFor="last-name">Last name</label>
+//       <input
+//         id="last-name"
+//         type="text"
+//         style={{ display: `block`, marginBottom: 16 }}
+//         {...register("Last name", { required: true, maxLength: 100 })}
+//       />
+
+//       <label htmlFor="email">Email</label>
+//       <input
+//         id="email"
+//         type="text"
+//         style={{ display: `block`, marginBottom: 16 }}
+//         {...register("Email", { required: true, pattern: /^\S+@\S+$/i })}
+//       />
+
+//       <label htmlFor="tel">Mobile number</label>
+//       <input
+//         id="tel"
+//         type="tel"
+//         style={{ display: `block`, marginBottom: 16 }}
+//         {...register("Mobile number", {
+//           required: true,
+//           minLength: 6,
+//           maxLength: 12,
+//         })}
+//       />
+
+//       <label htmlFor="title">Title</label>
+//       <select
+//         {...register("Title", { required: true })}
+//         style={{ display: `block`, marginBottom: 16 }}
+//       >
+//         <option value="Mr">Mr</option>
+//         <option value="Mrs">Mrs</option>
+//         <option value="Miss">Miss</option>
+//         <option value="Dr">Dr</option>
+//       </select>
+
+//       <input type="submit" />
+//     </form>
+//   )
+// }
