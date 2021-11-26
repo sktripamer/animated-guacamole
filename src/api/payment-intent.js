8c81202b080392ca49b0e6f0f0103879f535d2bf
sktripamer <42321951+sktripamer@users.noreply.github.com>
2021-11-25 22:20:18 -0800

    Update payment-intent.js

--- src/api/payment-intent.js
+++ src/api/payment-intent.js
@@ -1,5 +1,5 @@
 const stripe = require("stripe")(process.env.STRIPE_SECRET);
-const { amount, currency = "gbp" } = JSON.parse(event.body);
+const { amount, currency = "usd" } = JSON.parse(event.body);
 (async () => {
 try {
   const paymentIntent = await stripe.paymentIntents.create({
@@ -9,6 +9,6 @@ try {
 
   res.status(200).json({body:JSON.stringify(paymentIntent)})
 } catch (e) {
-  res.status(500).json({body:e})
+  res.json({body: 'error ' + e})
 }
 })()
