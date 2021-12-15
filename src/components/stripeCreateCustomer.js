ae319ffbf621cdabbcff6dbe7c11964af42c5d81
sktripamer <42321951+sktripamer@users.noreply.github.com>
2021-12-15 00:28:43 -0800

    Update stripeCreateCustomer.js

--- src/components/stripeCreateCustomer.js
+++ src/components/stripeCreateCustomer.js
@@ -6,7 +6,7 @@ export default function CustomerForm() {
   const [error, setError] = useState(null);
   const [processing, setProcessing] = useState("");
   const [disabled, setDisabled] = useState(true);
-  const [email, setEmail] = useState("");
+  //const [email, setEmail] = useState("");
   const nameForm = useRef(null);
   //const [clientSecret, setClientSecret] = useState("");
 
@@ -77,36 +77,62 @@ export default function CustomerForm() {
   };
 
   const handleSubmit = async (ev) => {
-    try {
+    // try {
 
     ev.preventDefault();
     setProcessing(true);
 
-    const form = nameForm.current
-    const email = form['firstname'].value
-    const request = await fetch("/api/create-customer", {
+    // const form = nameForm.current
+    // const email = form['firstname'].value
+    // const request = await fetch("/api/create-customer", {
+    //     method: 'POST',
+    //     headers: {
+    //         "Content-Type": "application/json",
+    //       },
+    //     body: JSON.stringify({
+    //       email
+    //     }),
+    //   });
+    const customer = await this.createCustomer();
+    console.log(customer?.id);
+    // const customer = (await request.json())
+    // console.log(customer)
+    // setError(null);
+    // setProcessing(false);
+    // setSucceeded(true);
+    // } catch (error){
+    // console.log('Failed to create customer');
+    // console.log(error);
+    // setProcessing(false);
+    // setError(error);
+    // }
+
+  };
+
+  async function createCustomer() {
+    try {
+      // Retrieve email and username of the currently logged in user.
+      // getUserFromDB() is *your* implemention of getting user info from the DB
+      const email = "sktripam.er@gmail.com"
+      if (!email) {
+        throw Error('Email or username not found.');
+      }
+      const request = await fetch('/api/create-customer', {
         method: 'POST',
-        headers: {
-            "Content-Type": "application/json",
-          },
         body: JSON.stringify({
           email
         }),
       });
-
-    const customer = (await request.json())
-    console.log(customer)
-    setError(null);
-    setProcessing(false);
-    setSucceeded(true);
-    } catch (error){
-    console.log('Failed to create customer');
-    console.log(error);
-    setProcessing(false);
-    setError(error);
+      const customer = (await request.json());
+      // Update your user in DB to store the customerID
+      // updateUserInDB() is *your* implementation of updating a user in the DB
+      return customer;
+    } catch (error) {
+      console.log('Failed to create customer');
+      console.log(error);
+      return null;
     }
-
-  };
+  }
 
   return (
     <form id="customer-form" ref={nameForm} onSubmit={handleSubmit}>
