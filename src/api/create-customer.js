11f2dd7e17e0c1239f85593d06c2a2c9719c6f2c
sktripamer <42321951+sktripamer@users.noreply.github.com>
2021-12-14 21:44:07 -0800

    Create create-customer.js

---
+++ src/api/create-customer.js
@@ -0,0 +1,27 @@
+const stripe = require("stripe")(process.env.STRIPE_SECRET);
+
+export default async function handler(body, res) {
+    if (!event.body) {
+      res.json({body: 'error invalid body'})
+    }
+    const body = JSON.parse(event.body);
+    const { email } = body;
+    
+    if (!email) {
+      res.json({body: 'error no email'})
+      return;
+    }
+
+    try {
+      // Create a new customer
+      const customer = await stripe.customers.create({
+        email
+      });
+      res.status(200).json({body:customer})
+
+    } catch (error) {
+      res.json({body: 'error ' + error})
+    }
+    
+}
+    
