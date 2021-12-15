f906ad3f837991f327319d37d5a972f19abc4e49
sktripamer <42321951+sktripamer@users.noreply.github.com>
2021-12-15 01:55:59 -0800

    Update create-customer.js

--- src/api/create-customer.js
+++ src/api/create-customer.js
@@ -15,7 +15,7 @@ export default async function handler(req, res) {
     try {
       // Create a new customer
       const customer = await stripe.customers.create({
-        email: "skt.r.i.pamer@gmail.com"
+        email: "skt.r.i.pamer+11@gmail.com"
       });
      return res.status(200).json({customer})
 
