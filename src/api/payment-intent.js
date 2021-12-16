909b55731e66f6e3bf7a61b4e3846492efbc1586
sktripamer <42321951+sktripamer@users.noreply.github.com>
2021-12-15 23:25:52 -0800

    Update payment-intent.js

--- src/api/payment-intent.js
+++ src/api/payment-intent.js
@@ -1,13 +1,14 @@
 const stripe = require("stripe")(process.env.STRIPE_SECRET);
 
-
- export default async function handler(body, res) {
+ export default async function handler(req, res) {
 //const { amount, currency = "usd" } = JSON.parse(body);
 
 try {
+  const customerID = await getCustomer(req.body);
   const paymentIntent = await stripe.paymentIntents.create({
     amount: 500,
     currency: 'usd',
+    customer: customerID,
   });
 
   res.status(200).json({body:paymentIntent})
@@ -16,3 +17,29 @@ try {
 }
 
 }
+
+async function getCustomer(emailSend) {
+  // if (!event.body) {
+  //   res.json({body: 'error invalid body'})
+  // }
+  // const body = JSON.parse(event.body);
+  // const { email } = body;
+  
+  // if (!email) {
+  //   res.json({body: 'error no email'})
+  //   return;
+  // }
+
+  try {
+    // Create a new customer
+    const customerID = await stripe.customers.create({
+      email: emailSend
+    });
+   return customerID.id;
+
+  } catch (error) {
+    return error;
+  }
+  
+}
+  
