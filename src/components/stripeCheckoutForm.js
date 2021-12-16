38812297dbd351e6f6be143408a0a8d83d45a922
sktripamer <42321951+sktripamer@users.noreply.github.com>
2021-12-16 01:40:14 -0800

    Update stripeCheckoutForm.js

--- src/components/stripeCheckoutForm.js
+++ src/components/stripeCheckoutForm.js
@@ -9,6 +9,8 @@ export default function CheckoutForm() {
   const [disabled, setDisabled] = useState(true);
   const [clientSecret, setClientSecret] = useState("");
   const nameForm = useRef(null);
+  const [successMessage, setSuccessMessage] = useState("");
+  const [showAlertBar, setShowAlertBar] = useState(true);
   const stripe = useStripe();
   const elements = useElements();
 
@@ -47,6 +49,27 @@ export default function CheckoutForm() {
       return intent;
     } catch (error) {
       console.log('Failed to create intent');
+      console.log(error);
+      return null;
+    }
+  }
+
+
+  async function getCustomerObj() {
+    try {
+      // Retrieve email and username of the currently logged in user.
+      // getUserFromDB() is *your* implemention of getting user info from the DB
+
+      const request = await fetch('/api/payment-intent', {
+        method: 'POST',
+        body: 'a',
+      });
+      const customerobj = (await request.json());
+      // Update your user in DB to store the customerID
+      // updateUserInDB() is *your* implementation of updating a user in the DB
+      return customerobj;
+    } catch (error) {
+      console.log('Failed to get customer');
       console.log(error);
       return null;
     }
@@ -92,6 +115,13 @@ export default function CheckoutForm() {
     },
   };
 
+  const getCustomer = async (ev) => {
+    ev.preventDefault();
+    const custobj = await getCustomerObj();
+    console.log(custobj)
+  }
+
+
   const handleChange = async (event) => {
     setDisabled(event.empty);
     setError(event.error ? event.error.message : "");
@@ -120,6 +150,7 @@ export default function CheckoutForm() {
   };
 
   return (
+    <div class='payment'>
     <form id="payment-form" ref={nameForm} onSubmit={handleSubmit}>
       <InputField label={'email'} name={'firstname'}/>
       <CardElement
@@ -149,5 +180,51 @@ export default function CheckoutForm() {
         Refresh the page to pay again.
       </p>
     </form>
+
+    <button id="getcustomer" onClick={getCustomer}>
+        <span id="button-text">
+            "get customer"
+        </span>
+      </button>
+    {/* <div class='rev-player'>
+    {"" !== successMessage
+    ? showAlertBar && (
+        
+
+     
+      <form id="payment-form" ref={nameForm} onSubmit={handleSubmit}>
+      <InputField label={'email'} name={'firstname'}/>
+      <CardElement
+        id="card-element"
+        options={cardStyle}
+        onChange={handleChange}
+      />
+      <button disabled={processing || disabled || succeeded} id="submit">
+        <span id="button-text">
+          {processing ? (
+            <div className="spinner" id="spinner"></div>
+          ) : (
+            "Pay now"
+          )}
+        </span>
+      </button>
+      {error && (
+        <div className="card-error" role="alert">
+          {error}
+        </div>
+      )}
+      <p className={succeeded ? "result-message" : "result-message hidden"}>
+        Payment succeeded, see the result in your
+        <a href={`https://dashboard.stripe.com/test/payments`}>
+          Stripe dashboard.
+        </a>
+        Refresh the page to pay again.
+      </p>
+    </form>
+
+      )
+    : ""}
+    </div> */}
+    </div>
   );
 }
